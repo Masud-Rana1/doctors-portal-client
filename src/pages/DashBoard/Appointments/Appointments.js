d0b7c202ea9faac68442f7a934af61c17e12b293
masud-rana-tech <developer.rana99@gmail.com>
2022-01-10 01:17:33 +0600

    removed date

--- src/pages/DashBoard/Appointments/Appointments.js
+++ src/pages/DashBoard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({date}) => {
     const {user, token} = useAuth();
     const [appointments, setAppointments] = useState([]);
     useEffect(()=>{
-      const url = `https://immense-retreat-64247.herokuapp.com/appointments?email=${user.email}&date=${date}`
+      const url = `https://immense-retreat-64247.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
           headers: {
             'authorization': `Bearer ${token}`
